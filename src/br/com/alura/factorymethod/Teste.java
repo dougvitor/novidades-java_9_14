8f93193cf52f0821a7ada034122453a9fb5a2ab0
Douglas Vitor França da Silva <douglas.vitor@totvs.com.br>
2021-03-08 20:24:47 -0300

    Aula 01.04 - Fabrica para Collections

--- src/br/com/alura/factorymethod/Teste.java
+++ src/br/com/alura/factorymethod/Teste.java
@@ -1,17 +1,30 @@
 package br.com.alura.factorymethod;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+//Novidade Java 9 - Factory Method para Collection.
 public class Teste {
 
 	public static void main(String[] args) {
-		ArrayList<String> nomes = new ArrayList<>();
-		nomes.add("primeiroNome");
-		nomes.add("segundoNome");
-		nomes.add("terceiroNome");
-		List<String> nomesImutavel = Collections.unmodifiableList(nomes);
-		System.out.println(nomesImutavel);
+		/*
+			ArrayList<String> nomes = new ArrayList<>();
+			nomes.add("primeiroNome");
+			nomes.add("segundoNome");
+			nomes.add("terceiroNome");
+			List<String> nomesImutavel = Collections.unmodifiableList(nomes);
+			System.out.println(nomesImutavel);
+		*/
+		
+		List<String> listaImutavel = List.of("PrimeiroNome", "SegundoNome", "TerceiroNome");
+		
+		Set<String> setImutavel = Set.of("QuartoNome", "QuintoNome", "SextoNome");
+		
+		Map<String, String> mapImutavel = Map.of("SetimoNome", "José", "NonoNome", "João", "DecimoNome", "Maria");
+		
+		System.out.println(listaImutavel);
+		System.out.println(setImutavel);
+		System.out.println(mapImutavel);
 	}
 }
