2f22813104913db9368622dcf2a8768b18006d3e
Douglas Vitor França da Silva <douglas.vitor@totvs.com.br>
2021-03-12 12:57:17 -0300

    Aula 4.06 - Criando o nosso Subscriber.

--- src/br/com/alura/reactive/streams/TesteEnvioNaoBloqueante.java
+++ src/br/com/alura/reactive/streams/TesteEnvioNaoBloqueante.java
@@ -2,34 +2,35 @@ package br.com.alura.reactive.streams;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Scanner;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.SubmissionPublisher;
 
 public class TesteEnvioNaoBloqueante {
 	
 	public static void main(String[] args) {
-
-		final ExecutorService threadPool = Executors.newFixedThreadPool(3);
 		
-		try(SubmissionPublisher<NotaFiscal> publisher = new SubmissionPublisher<>(threadPool, 3)){
+		try(SubmissionPublisher<NotaFiscal> publisher = new SubmissionPublisher<>()){
+			try(Scanner scan = new Scanner(System.in)){
+				var notasFiscais = List.of(
+						new NotaFiscal("João", new BigDecimal("39.99"), LocalDate.now()),
+						new NotaFiscal("Renata", new BigDecimal("41.20"), LocalDate.now()),
+						new NotaFiscal("Fernanda", new BigDecimal("15.00"), LocalDate.now())
+				    );
+				
+				
+				NotaFiscalSubscriber subscriber = new NotaFiscalSubscriber();
+				publisher.subscribe(subscriber);
+				
+				notasFiscais.forEach(nota -> publisher.submit(nota));
+				
+				System.out.println("Você irá receber a nota fiscal no seu e-mail");
+				
+				scan.nextLine();
+			}
 			
-			var notaFiscal = new NotaFiscal("João", new BigDecimal("39.99"), LocalDate.now());
-
-			var ws = new NotaFiscalWSClient();
-			
-			publisher.consume(ws::enviar);
-			publisher.consume(data -> System.out.println(String.format("1 - Thread Name: %s", Thread.currentThread().getName())));
-			publisher.consume(data -> System.out.println(String.format("2 - Thread Name: %s", Thread.currentThread().getName())));
-			publisher.submit(notaFiscal);
-			
-			System.out.println("Você irá receber a nota fiscal no seu e-mail");
-			
-			Scanner scan = new Scanner(System.in);
-			scan.nextLine();
-			scan.close();
 		}
+		
 	}
 
 }
