4d85746d7e3caff1bd50c85411f5682b9f64e7b6
Douglas Vitor França da Silva <douglas.vitor@totvs.com.br>
2021-03-12 12:19:50 -0300

    Aula 4.04 - Trabalhando com diferentes threads.

--- src/br/com/alura/reactive/streams/NotaFiscalWSClient.java
+++ src/br/com/alura/reactive/streams/NotaFiscalWSClient.java
@@ -8,7 +8,11 @@ public class NotaFiscalWSClient {
 		try {
 			System.out.println("Emitindo nota Fiscal");
 			Thread.sleep(5000);
+			
+			System.out.println(String.format("3 - Thread Name: %s", Thread.currentThread().getName()));
+			
 			System.out.format("Nota Fiscal emitida em nome de %s na data de %s", nf.getNome(), nf.getData());
+			
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
--- src/br/com/alura/reactive/streams/TesteEnvioNaoBloqueante.java
+++ src/br/com/alura/reactive/streams/TesteEnvioNaoBloqueante.java
@@ -3,19 +3,25 @@ package br.com.alura.reactive.streams;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.SubmissionPublisher;
 
 public class TesteEnvioNaoBloqueante {
 	
 	public static void main(String[] args) {
+
+		final ExecutorService threadPool = Executors.newFixedThreadPool(3);
 		
-		try(SubmissionPublisher<NotaFiscal> publisher = new SubmissionPublisher<>()){
+		try(SubmissionPublisher<NotaFiscal> publisher = new SubmissionPublisher<>(threadPool, 3)){
 			
 			var notaFiscal = new NotaFiscal("João", new BigDecimal("39.99"), LocalDate.now());
 
 			var ws = new NotaFiscalWSClient();
 			
 			publisher.consume(ws::enviar);
+			publisher.consume(data -> System.out.println(String.format("1 - Thread Name: %s", Thread.currentThread().getName())));
+			publisher.consume(data -> System.out.println(String.format("2 - Thread Name: %s", Thread.currentThread().getName())));
 			publisher.submit(notaFiscal);
 			
 			System.out.println("Você irá receber a nota fiscal no seu e-mail");
